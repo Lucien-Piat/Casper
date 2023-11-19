cecd8d4b33e0d0f1059e44a104f937cb664f0168
SaraSpieler <150816503+SaraSpieler@users.noreply.github.com>
2023-11-19 18:54:15 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -12,6 +12,19 @@ def affichage_menu():
     print("2 : Régle du jeu") 
     print("0 : Quitter")
 
+def menu_deplacement() :
+    print(" -------------------------------------------")
+    print("|          Ou voulez vous allez             |")
+    print("|                                           |")
+    print("|    1   :    Gauche                        |") 
+    print("|    2   :    Droite                        |")
+    print("|    3   :    Monter                        |")
+    print("|    4   :    Descendre                     |")
+    print("|                                           |")
+    print("|    0  :    Quitter                        |")
+    print(" -------------------------------------------")
+   
+
 def menu():
     import sys
     choix=int(input("\nChoisissez un nombre : "))
@@ -19,7 +32,7 @@ def menu():
     if choix==0 : #sortir du jeu
         sys.exit()
     elif choix==1 : #lancer le jeu
-        print("ok")
+        menu_deplacement()
     elif choix==2 : #Regle du jeu 
         print("Bienvenue dans Fantome escape")
         print("\nDans ce jeu nous avons Casper, un gentil fantome prisonnier dans un labyrinthe avec des etres \nmalefiques qui veulent le garder prisonnier dans le chateausper")
@@ -46,3 +59,123 @@ def menu():
 
 affichage_menu()
 menu()
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+tableau = { 'pinte' : [[0,1], [0,3], [1,2], [1,2], [4,1]], 
+            'bib_c' : [ [0,2], [2,2], [2,3]], 
+            'maitre' : [[2,1]],
+            'savant' : [[4,2]],
+            'fantome' : [[1,1]],
+            'no' : [[0,0], [1,0], [2,0],[4,0], [3,1],[3,2],[3,3],[4,3],[0,4],[2,4],[4,4]],
+            'gauche' : [[0,1],[1,1],[2,1],[0,2],[1,2],[2,2],[0,3],[1,3],[2,3]],
+            'droite' : [[2,1], [4,1], [2,2], [4,2], [2,3], [4,3]],
+            'reception' : [[1,4]], 
+            'paradis' : [[3,0]]}
+
+for i in tableau.keys():
+    print(i, ' : ', tableau[i])
+
+
+def menu_deplacement() :
+    print(" -------------------------------------------")
+    print("|          Ou voulez vous allez             |")
+    print("|                                           |")
+    print("|    1   :    Gauche                        |") 
+    print("|    2   :    Droite                        |")
+    print("|    3   :    Monter                        |")
+    print("|    4   :    Descendre                     |")
+    print("|                                           |")
+    print("|    0  :    Quitter                        |")
+    print(" -------------------------------------------")
+
+def reception_deplacement(dictionnaire,nb):
+    """"Renvois ce qui ce passe quand on part de la case reception, 
+            gauche => case [0:3]
+            droite => case [2,3] 
+        Donc on vas sur la case et on change les coordonnee du fantome.
+    """
+    fantome = dictionnaire['fantome']
+    reception= dictionnaire['reception']
+    if fantome == reception : 
+        fantome=[]
+        if nb == 1 : #gauche 
+            fantome.append([0,3])
+            print("Vous vennais d'entre dans la case ",fantome, "\nVous avez quitter la recepetion")
+
+        elif nb == 2 : #droite
+            fantome.append([2,3])
+            print("Vous vennais d'entre dans la case ",fantome, "\nVous avez quitter la recepetion")
+        dictionnaire['fantome']=fantome
+    return dictionnaire
+
+
+def monter_decendre(dictionnaire, nb): 
+    gauche = dictionnaire['gauche']
+    droite = dictionnaire ['droite']
+    fantome = dictionnaire['fantome']
+    if nb == 1 : 
+        x = fantome[0]
+        y = fantome[1]
+        res=[x-1,y]
+        fantome=[]
+        fantome.append(res)
+    elif nb == 2 :
+        x = fantome[0]
+        y = fantome[1]
+        res=[x+1,y]
+        fantome=[]
+        fantome.append(res)
+
+    dictionnaire['fantome']=fantome
+    
+def extremite(dictionnaire):
+    fantome = dictionnaire['fantome']
+    if fantome[0][0]==0 : #si x = 0, extremité gauche (ne peut plus aller a gauche)
+        print("tu ne peut plus aller a gauche")
+        return None
+    elif fantome[0][0]==4 :#si x = 4, extremité droite (ne peut plus aller a droite)
+        print("tu ne peut plus aller a droite ")
+        return None
+    else : 
+        return True 
+
+
+def move(dictionnaire): 
+    gauche = dictionnaire['gauche']
+    droite = dictionnaire ['droite']
+    fantome = dictionnaire['fantome']
+    print("gauche : ", gauche, "\ndroite : ", droite, "\nCasper : ", fantome)
+    menu_deplacement()
+    choix=int(input("\nChoisissez un nombre : "))
+    print()
+
+    if choix == 1 : #Gauche 
+        reception_deplacement(dictionnaire,choix) 
+
+    if choix == 2 : #Droite
+        reception_deplacement(dictionnaire,choix) 
+
+    if choix == 3 : #Monter
+        #print(fantome[0][1])
+        compteur = fantome[0][1] - 1
+        print(compteur)
+        if compteur <= 1 : #permet de tester si on est pres ou pas du bord!! (quand on est pres du bord ca fonctionne )
+            print("vous ne pouvez plus monter, Choisissez une autre option") 
+            #menu_deplacement()
+            #choix==int(input("\nChoisissez un nombre : "))
+
+
+
+        """il manque plein de chose JE VAIS MOURIRE 
+        une fois qu'on choisi droite ou gauche, suffit de changer fantome[0],
+        plus check si possible d'aller dans cette direction"""
+        
+    return dictionnaire
+
+
+move(tableau)
+for i in tableau.keys():
+    print(i, ' : ', tableau[i])
+
